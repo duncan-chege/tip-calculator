8a265b6751d4c5a605f64932c342572f9b572c83
viisualworks <dshege4@gmail.com>
2024-11-29 14:37:17 +0300

    add an EntrySection component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
+import EntrySection from './components/EntrySection'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <EntrySection />
   )
 }
 
---
+++ src/components/EntrySection.tsx
@@ -0,0 +1,9 @@
+const EntrySection = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Entry</h1>
+        </div>
+    )
+}
+
+export default EntrySection;
